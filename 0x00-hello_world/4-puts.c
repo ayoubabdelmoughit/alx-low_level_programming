e1794d591b0e80a9c8683c1d244c3ce4e286c37c
ayoubabdelmoughit <ayoubabdelmoughit75@gmail.com>
2023-02-16 08:09:37 -0800

    add new task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-
-
 /**
- * main - prints "Programming is like building a multilingual
- * puzzle, followed by a newline."
- * return 0.
+ * main - Entry point
+ * Description: print "\"Programming is like building a multilingual puzzle"
+ * Return: 0
  */
-
 int main(void)
-
 {
-        puts("\"Programming is like building a multilingual puzzle");
-
-        return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
