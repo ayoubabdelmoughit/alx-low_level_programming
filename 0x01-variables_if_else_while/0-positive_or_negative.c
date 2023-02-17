fd0e703b47699a41c2fc2d500628b78221b2ce07
ayoubabdelmoughit <ayoubabdelmoughit75@gmail.com>
2023-02-17 04:10:31 -0800

    add new task

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void)
+{
+int n;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+return (0);
+}
+
