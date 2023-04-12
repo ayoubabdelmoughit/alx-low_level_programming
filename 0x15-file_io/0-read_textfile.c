176a569606632f3c1197109c54b7a70cbde841d6
ayoub abdelmoughit <ayoubabdelmoughit75@gmail.com>
2023-04-12 16:24:02 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,30 +13,29 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	 ssize_t open, red, write;
-	char *buffer;
+	 ssize_t open, read, write;
+	char *zagay;
 
-	if (filename == NULL)
+	if (mlaghbeb == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer ==  NULL)
+	mlaghbeb = malloc(sizeof(char) * letters);
+	if (mlaghbeb ==  NULL)
 		return (0);
 
 	open = open(filename, O_RDONLY);
-	read = read(open, buffer, letters);
-	write = write(STDOUT_FILENO, buffer, read);
+	read = read(open, mlaghbeb, letters);
+	write = write(STDOUT_FILENO, mlaghbeb, read);
 
 	if (open == -1 || read == -1 || write == -1 || write != read)
 	{
-		free(buffer);
+		free(mlaghbeb);
 		return (0);
 	}
 
-	free(buffer);
+	free(mlaghbeb);
 	close(open);
 
 	return (write);
 
 }
-
